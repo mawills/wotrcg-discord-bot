2e3ef9f4d06249e02f901ff0424892540ac43d32
Matt Wills <mattwillscode@gmail.com>
2024-05-24 21:12:19 -0400

    the bot lives!

--- bot/src/bot.ts
+++ bot/src/bot.ts
@@ -1,4 +1,4 @@
-import { Client, Events, GatewayIntentBits, Message, TextChannel } from 'discord.js';
+import { Client, Events, GatewayIntentBits, Message } from 'discord.js';
 import Messenger from './messenger.js';
 
 export default class Palantir {
@@ -10,7 +10,11 @@ export default class Palantir {
     }
 
     initializeClient(token: string) {
-        const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+        const client = new Client({ intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent
+        ] });
         client.once(Events.ClientReady, (readyClient: Client) => {
             if (readyClient.user) {
                 console.log(`Ready! Logged in as ${readyClient.user.tag}`);
@@ -18,8 +22,8 @@ export default class Palantir {
                 console.error('client user not found.');
             }
         });
-        client.on('message', (msg: Message) => {
-            console.log(msg.content);
+
+        client.on('messageCreate', (msg: Message) => {
             const messenger = new Messenger(client, msg);
             messenger.send();
         })
